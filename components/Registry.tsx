900ee7fb50fb8afdf044053342aa8ba922a704cf
陆子虚 <lurongv@qq.com>
2025-06-06 13:46:42 +0800

    ✨feature: 添加本地化配置

--- components/Registry.tsx
+++ components/Registry.tsx
@@ -2,18 +2,13 @@
 
 import { FC, ReactNode } from "react"
 import { AntdRegistry } from "@ant-design/nextjs-registry"
-import { HeroUIProvider } from "@heroui/react"
+import { HeroUIProvider, ToastProvider } from "@heroui/react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ConfigProvider } from "antd"
 import zhCN from "antd/locale/zh_CN"
 import dayjs from "dayjs"
 
 import "dayjs/locale/zh-cn"
-
-import { MessageInstance } from "antd/es/message/interface"
-import useMessage from "antd/es/message/useMessage"
-
-import { IsBrowser } from "@/constants"
 
 dayjs.locale("zh-cn")
 
@@ -31,22 +26,17 @@ const queryClient = new QueryClient({
     },
 })
 
-declare global {
-    var message: MessageInstance
-}
-
 const Registry: FC<RegistryProps> = props => {
     const { children } = props
-    const [message, contextHolder] = useMessage()
-
-    if (IsBrowser) globalThis.message = message
 
     return (
         <QueryClientProvider client={queryClient}>
             <AntdRegistry hashPriority="high">
                 <ConfigProvider locale={zhCN} theme={{ token: { fontFamily: "Source Han Sans VF" } }}>
-                    {contextHolder}
-                    <HeroUIProvider>{children}</HeroUIProvider>
+                    <HeroUIProvider locale="zh-CN">
+                        <ToastProvider />
+                        {children}
+                    </HeroUIProvider>
                 </ConfigProvider>
             </AntdRegistry>
         </QueryClientProvider>

ac4402353854999dbee55fde79d0473d867d4811
陆子虚 <lurongv@qq.com>
2025-06-26 15:09:36 +0800

    ✨feature: 关闭 mutation 的默认重试

--- components/Registry.tsx
+++ components/Registry.tsx
@@ -24,6 +24,9 @@ const queryClient = new QueryClient({
             refetchOnWindowFocus: false,
             refetchOnMount: false,
         },
+        mutations: {
+            retry: 0,
+        },
     },
 })
 

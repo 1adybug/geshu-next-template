8eadda30101d5264bd1f93565e4793fb6ee22512
陆子虚 <lurongv@qq.com>
2025-06-27 08:40:52 +0800

    ✨feature: 修改 JWT_SECRET 常量的命名为 JwtSecrect

--- server/sign.ts
+++ server/sign.ts
@@ -1,9 +1,9 @@
 import { SignJWT } from "jose"
 
-import { JWT_SECRET } from "@/constants"
+import { JwtSecrect } from "@/constants"
 
 export async function sign(id: string) {
-    const secretKey = new TextEncoder().encode(JWT_SECRET)
+    const secretKey = new TextEncoder().encode(JwtSecrect)
     const token = await new SignJWT({ id }).setProtectedHeader({ alg: "HS256" }).setIssuedAt().setExpirationTime("1 day").sign(secretKey)
     return token
 }

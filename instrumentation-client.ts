0822af094c38f296209267fd1fc545bb82471e70
陆子虚 <lurongv@qq.com>
2025-11-25 09:09:45 +0800

    ✨feature: 升级依赖

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -3,7 +3,6 @@ import dayjs from "dayjs"
 import { createRequestFn, getErrorMessage } from "deepsea-tools"
 import { isRedirectError } from "next/dist/client/components/redirect-error"
 
-import "@ant-design/v5-patch-for-react-19"
 import "dayjs/locale/zh-cn"
 
 createRequestFn.use(async (context, next) => {
--- instrumentation.ts
+++ instrumentation.ts
@@ -1,7 +1,6 @@
 import dayjs from "dayjs"
 
 export async function register() {
-    await import("@ant-design/v5-patch-for-react-19")
     await import("dayjs/locale/zh-cn")
     dayjs.locale("zh-cn")
 }

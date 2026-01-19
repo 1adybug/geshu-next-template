0fa811e64cd9765354a9b7f1b68680a15ccdd514
陆子虚 <lurongv@qq.com>
2026-01-19 09:48:53 +0800

    ✨feature: format

--- examples/rp-with-users/server/storage.ts
+++ examples/rp-with-users/server/storage.ts
@@ -1,5 +1,5 @@
-import { access, mkdir, readFile, writeFile } from "fs/promises"
-import { dirname, join } from "path"
+import { access, mkdir, readFile, writeFile } from "node:fs/promises"
+import { dirname, join } from "node:path"
 
 import { v4 as uuid } from "uuid"
 

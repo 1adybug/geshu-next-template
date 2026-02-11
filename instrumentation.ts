b3140ad41abc56bba6df932d0167a1a99e50b544
陆子虚 <lurongv@qq.com>
2026-02-11 14:17:40 +0800

    ✨feature: 移除同步数据的初始化命令

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,16 +1,1 @@
-import { existsSync } from "node:fs"
-
-import { spawnAsync } from "soda-nodejs"
-
-import { DatabaseUrl } from "./prisma.config"
-
-export const runtime = "nodejs"
-
-export async function register() {
-    if (!existsSync(DatabaseUrl.replace(/^file:/, ""))) {
-        await spawnAsync("npx prisma db push", {
-            stdio: "inherit",
-            shell: true,
-        })
-    }
-}
+export async function register() {}

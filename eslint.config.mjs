2f0dfad3b9fceb8dbd8df2fee81e69f87fde457b
陆子虚 <lurongv@qq.com>
2026-07-22 14:35:54 +0800

    chore: update shared ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,1 +1,13 @@
-export { default } from "@1adybug/eslint"
+import config from "@1adybug/eslint"
+
+const eslintConfig = [
+    ...config,
+    {
+        files: ["shared/**/*.{js,jsx,mjs,cjs,ts,tsx,mts,cts}"],
+        rules: {
+            "prefer-arrow-callback": "off",
+        },
+    },
+]
+
+export default eslintConfig
